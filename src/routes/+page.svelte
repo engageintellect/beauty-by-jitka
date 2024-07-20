e630d8ffdd9f2afe34bd39d23317366e1355b1a3
engageintellect <jc9361@gmail.com>
2024-07-19 17:14:13 -0700

    adjusting layout flicker on initial render using short delays on home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import type { PageData } from './$types.js';
 	export let data: PageData;
+	import { onMount } from 'svelte';
 	import AboutCard from '$lib/components/ui/AboutCard.svelte';
 	import Form from '$lib/components/ui/Form.svelte';
 	import Hero from '$lib/components/ui/Hero.svelte';
@@ -9,19 +10,29 @@
 	import SocialLinks from '$lib/components/ui/SocialLinks.svelte';
 	import Quote from '$lib/components/ui/Quote.svelte';
 	import Locations from '$lib/components/ui/Locations.svelte';
+
+	let showComponents = false;
+
+	onMount(() => {
+		setTimeout(() => {
+			showComponents = true;
+		}, 10);
+	});
 </script>
 
 <div class="flex flex-col gap-10 md:gap-20">
-	<Hero />
-	<AboutCard />
-	<SkillsCard />
-	<Quote />
-	<Difference />
-	<SocialLinks />
-	<Locations />
-	<div
-		class="mb-5 flex h-full w-full flex-col justify-center gap-5 rounded-lg p-5 transition-all duration-500 sm:min-h-full md:mb-20 md:flex-row md:justify-center md:border md:shadow-lg"
-	>
-		<Form data={data.form} />
-	</div>
+	{#if showComponents}
+		<Hero />
+		<AboutCard />
+		<SkillsCard />
+		<Quote />
+		<Difference />
+		<SocialLinks />
+		<Locations />
+		<div
+			class="mb-5 flex h-full w-full flex-col justify-center gap-5 rounded-lg p-5 transition-all duration-500 sm:min-h-full md:mb-20 md:flex-row md:justify-center md:border md:shadow-lg"
+		>
+			<Form data={data.form} />
+		</div>
+	{/if}
 </div>
